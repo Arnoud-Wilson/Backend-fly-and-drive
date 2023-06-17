0e509c595afa04cf9531c85fd83d026ec9d8ac91
Arnoud-Wilson <arnoud608@gmail.com>
2023-06-17 08:59:55 +0200

    finished assignment

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ package src;
 public class Main {
 
     public static void main(String[] args) {
+        Car seat = new Car(130, 550);
+        FlyingCar superSeat = new FlyingCar(350, 550);
+        Plane boeing = new Plane(500, 750);
 
+        seat.startEngine();
+        seat.turnOffEngine();
 
+        superSeat.accelarate();
+        superSeat.changeHeight();
 
-
+        boeing.land();
+        boeing.takeOff();
     }
-
 }
